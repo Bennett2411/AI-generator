7fa559e8372a7d240863937bd5d5eb883dc98e38
Bennett2411 <sharna.bennett@hotmail.com>
2025-03-28 12:06:32 +0000

    resolved error with axios

--- src/java.js
+++ src/java.js
@@ -12,8 +12,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector('#userInstructions');
   let apiKey = `e2f90841bba6oe00351f9aa2e3877b4t`;
   let prompt = `Generate a poem in English about ${instructionsInput.value}`;
-  let context = `User Instructions: you are a romanic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br>. Make sure to follow the user instructions.
-  `;
+  let context = `User Instructions: you are a romanic poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br>. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log('generating poem');
